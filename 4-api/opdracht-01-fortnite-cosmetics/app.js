97f46abb45adefc997601c2ff31da1eb4bbbf143
Lukaszvdblink <9025326@student.zadkine.nl>
2025-11-13 13:27:10 +0100

    opdracht 4 api

--- 4-api/opdracht-01-fortnite-cosmetics/app.js
+++ 4-api/opdracht-01-fortnite-cosmetics/app.js
@@ -1,22 +1,22 @@
-fetch(`https://fortnite-api.com/v2/cosmetics/new`)
-.then((response) => response.json())
-.then((data) => {
+fetch('https://fortnite-api.com/v2/cosmetics/new')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.createElement('div');
+    container.classList.add('grid');
+    document.body.appendChild(container);
 
-
-
-for (let d of data.data.items.br){
-
-console.log(d.name);
-
-
-
-
-
-}
-
-
-
-
-
-});
+    for (let item of data.data.items.br) {
+      const card = document.createElement('div');
+      card.classList.add('card');
+      card.innerHTML = `
+        <img src="${item.images.icon}" alt="${item.name}">
+        <h3>${item.name}</h3>
+      `;
+      container.appendChild(card);
+    }
+  })
+  .catch(error => {
+    console.log('Er ging iets mis:', error);
+    document.body.innerHTML = '<p>Sorry, data kon niet geladen worden.</p>';
+  });
 
